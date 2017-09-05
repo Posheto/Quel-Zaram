5ed3b933acab71e0d9c2e26b9dd5387e76590fa1
Posheto <804108511@qq.com>
2017-09-05 23:31:27 +0800

    Quest 1 change location !
    ● Change language location into English
    
    I forgot my computer showing Chinese will be garbled...

--- Github_Learning/Test1.java
+++ Github_Learning/Test1.java
@@ -1,4 +1,5 @@
 import java.util.Date;
+import java.util.Locale;
 import java.util.Scanner;
 import java.text.*;
 
@@ -7,7 +8,7 @@ public class Test1 {
 	public static void main (String[] args) {
 		Date date = null;
 		SimpleDateFormat input = new SimpleDateFormat("yyyy MM dd");
-		SimpleDateFormat output = new SimpleDateFormat("E");
+		SimpleDateFormat output = new SimpleDateFormat("EEE", Locale.ENGLISH);
 		Scanner scanner = new Scanner(System.in);
 		String s = scanner.nextLine();
 		try {
@@ -16,14 +17,15 @@ public class Test1 {
 			System.err.println(e.getMessage());
 		}
 		int i = 0;
+		System.out.println(output.format(date.getTime()).toString());
 		switch (output.format(date.getTime()).toString()) {
-		case "星期日": i = 0; break; 
-		case "星期一": i = 1; break; 
-		case "星期二": i = 2; break; 
-		case "星期三": i = 3; break; 
-		case "星期四": i = 4; break; 
-		case "星期五": i = 5; break; 
-		case "星期六": i = 6; break; 
+		case "Sun": i = 0; break; 
+		case "Mon": i = 1; break; 
+		case "Tue": i = 2; break; 
+		case "Wed": i = 3; break; 
+		case "Thu": i = 4; break; 
+		case "Fri": i = 5; break; 
+		case "Sat": i = 6; break; 
 		}
 		System.out.println(i);
 		scanner.close();
