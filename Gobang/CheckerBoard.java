50d5c16cfa72a61f97c4a3812de61caf8196084a
Posheto <804108511@qq.com>
2017-09-13 12:58:24 +0800

    Gobang Online ver 0.1.2
    ● Add the UID
    ● Standardized board

--- Gobang/CheckerBoard.java
+++ Gobang/CheckerBoard.java
@@ -1,7 +1,9 @@
 package Gobang;
 
+import java.awt.BasicStroke;
 import java.awt.Font;
 import java.awt.Graphics;
+import java.awt.Graphics2D;
 import java.awt.GraphicsConfiguration;
 import java.awt.HeadlessException;
 import java.awt.Toolkit;
@@ -49,6 +51,8 @@ public class CheckerBoard extends JFrame implements MouseListener {
 
 	public void paint(Graphics g) {
 		
+		Graphics2D rim = (Graphics2D)g;
+		
 		g.drawImage(background, 1, 27, this);
 		g.setFont(new Font("Consolas", Font.BOLD, 25));
 		g.drawString("Game Info", 150, 60);
@@ -56,18 +60,25 @@ public class CheckerBoard extends JFrame implements MouseListener {
 		g.drawString("Black's time : Unlimited", 30, 475);
 		g.drawString("White's time : Unlimited", 252, 475);
 		
-		g.drawLine(11, 77, 370, 77);
-		g.drawLine(11, 77, 11, 436);
-		g.drawLine(11, 436, 370, 436);
-		g.drawLine(370, 77, 370, 436);
+		rim.setStroke(new BasicStroke(3.0f));
+		rim.drawLine(23, 89, 358, 89);
+		rim.drawLine(23, 89, 23, 424);
+		rim.drawLine(23, 424, 358, 424);
+		rim.drawLine(358, 89, 358, 424);
+		rim.setStroke(new BasicStroke(1.0f));
 		
-		for (int i = 1; i < 19; i++) {
-			g.drawLine(11, 77 + i*19, 370, 77 + i*19);
+		for (int i = 1; i < 15; i++) {
+			g.drawLine(23, 89 + i*24, 358, 89 + i*24);
+		}
+		for (int i = 1; i < 15; i++) {
+			g.drawLine(23 + i*24, 89, 23 + i*24, 424);
 		}
 		
-		for (int i = 1; i < 19; i++) {
-			g.drawLine(11 + i*19, 77, 11 + i*19, 436);
-		}
+		g.fillOval(90, 157, 10, 10);
+		g.fillOval(282, 157, 10, 10);
+		g.fillOval(90, 349, 10, 10);
+		g.fillOval(282, 349, 10, 10);
+		g.fillOval(186, 253, 10, 10);
 		
 	}
 	
